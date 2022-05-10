68b124941cebb495dc3b3ca00a2d276efcaf43d6
zsiyed <69491969+zsiyed@users.noreply.github.com>
2022-05-10 00:48:25 -0700

    adding abt us page

---
+++ zeds_commit/st_abt_us.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("About Us")
+st.header("Project Members")
+st.subheader("Project Lead: Victor Thai")
+
+print("Victor is a second-year at UC San Diego, majoring in Data Science and minor in Cognitive Science. He is interested in new applications of data science to create meaning with information. He believes that it's important to turn what others perceive as worthless to useful meaning within computer science and data science-related topics.")
+
+st.subheader("Michael Chen")
+
+print("Michael is a second-year student at UC San Diego, majoring in Data Science and Minor in Management Science. Michael is also involved in the Finance Committee in UTS at UCSD.")
+
+st.subheader("Stephanie Chavez")
+
+print("Stephanie is a second-year student at UC San Diego, majoring in Data Science.")
+
+st.subheader("Zed Siyed")
+
+print("Zed is a first-year student at UC San Diego, majoring in Computer Science. Zed is also a member of Triton Consulting Group as the VP Tech Consulting. Some of Zed's hobbies are sports analytics and weightlifting.")
+
