9d1d888256c710452d9e59fbffed73655d219a6d
zsiyed <69491969+zsiyed@users.noreply.github.com>
2022-05-10 15:01:18 -0700

    adding abt us pics

--- zeds_commit/st_abt_us.py
+++ zeds_commit/st_abt_us.py
@@ -14,12 +14,20 @@ v_col1, v_col2 = st.columns(2)
 with v_col1:
     st.subheader("Project Lead: Victor Thai")
     st.write("Victor is a second-year at UC San Diego, majoring in Data Science and minor in Cognitive Science. He is a member of CASA at UCSD and enjoys weightlifting to relieve stress.")
-    
+with v_col2:
+    st.subheader("")
+    v_img = Image.open("victor_headshot.jpeg")
+    st.image(v_img, width = 200)
+
 m_col1, m_col2 = st.columns(2)
 with m_col1:
     st.subheader("Michael Chen")    
     st.write("Michael is a second-year student at UC San Diego, majoring in Data Science and Minor in Management Science. Michael is also involved in the Finance Committee in UTS at UCSD.")
-
+with m_col2:
+    st.subheader("")
+    m_img = Image.open("michael_headshot.jpeg")
+    st.image(m_img, width = 200)
+    
 s_col1, s_col2 = st.columns(2)
 with s_col1:
     st.subheader("Stephanie Chavez")
