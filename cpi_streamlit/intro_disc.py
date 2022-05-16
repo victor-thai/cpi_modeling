85e2321d48ccf3998a82afa5c67b5e2b3e6bf56b
victhaaa <v3thai@ucsd.edu>
2022-05-16 09:29:42 -0700

    model rmse

--- cpi_streamlit/intro_disc.py
+++ cpi_streamlit/intro_disc.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from prophet import Prophet
+# from prophet import Prophet
 from PIL import Image
 from statsmodels.tsa.statespace.varmax import VARMAX
 from timeit import default_timer as timer
